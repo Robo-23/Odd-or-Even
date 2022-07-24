5dac2df0afdf96e513abcfb6beb4dda124cfb112
Robo-23 <109914532+Robo-23@users.noreply.github.com>
2022-07-24 22:45:13 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
+mkdir -p ~/.streamlit/
 
+echo "\
+[general]\n\
+email = \"21f1004734@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
