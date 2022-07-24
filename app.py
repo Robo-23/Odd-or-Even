bc3679e1dbaf65ccbc4f7dab630e2b4320708cbe
Robo-23 <109914532+Robo-23@users.noreply.github.com>
2022-07-24 23:06:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,11 @@ This app tells if the number is Odd or Even
 #Get Input
 
 st.header('User Input Parameters')
-
-def user_input_features():
-    number = st.number_input("GIVEN_NUMBER",min_value=0,max_value=100,step=1)
-    return number
+number = st.number_input("GIVEN_NUMBER")
 
 if number % 2==0:
-    st.write('Number is Even')
+    st.subheader('Number is Even')
 else:
-    st.write('Number is Odd')
+    st.subheader('Number is Odd')
 
 
